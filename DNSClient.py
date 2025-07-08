6da580a42709483d60c0c0f703bb6982b10764b6
Angel-MtzV <anm002@ucsd.edu>
2025-07-08 13:33:49 -0700

    updated local DNS server IP

--- DNSClient.py
+++ DNSClient.py
@@ -1,7 +1,7 @@
 import dns.resolver
 
 # Set the IP address of the local DNS server and a public DNS server
-local_host_ip = '68.105.28.11'
+local_host_ip = '68.105.29.11'
 real_name_server = '8.8.8.8' # Research public DNS servers to find a valid DNS server IP address to use
 
 
